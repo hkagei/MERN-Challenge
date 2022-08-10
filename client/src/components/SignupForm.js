59526c5804308084993cecd266dc0463534a5e5e
hkagei <k_dawg_1022@hotmail.com>
2022-08-09 23:31:00 -0600

    update login functionality

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -37,7 +37,7 @@ const SignupForm = () => {
       });
 
       console.log(data);
-      Auth.login(data.addUser.login.token);
+      Auth.login(data.addUser.token);
       
     } catch (err) {
       console.error(err);
