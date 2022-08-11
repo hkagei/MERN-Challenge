927a0b311147759aef4e12c96b56d3701227d987
hkagei <k_dawg_1022@hotmail.com>
2022-08-11 00:37:50 -0600

    fix file structure

--- server/server.js
+++ server/server.js
@@ -18,14 +18,14 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/"))
-})
-
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/"))
+})
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
